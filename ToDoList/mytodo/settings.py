ce7860090ae594194f5e4a9f56a3997e34e30977
ryukyunghye <ryukyung123@naver.com>
2022-07-31 16:47:55 +0900

    feat: 전체 조회 API

--- ToDoList/mytodo/settings.py
+++ ToDoList/mytodo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g143#hzod3n-it(jo15rryn$1hse4rc-0a1#-6-cm!#uub1eb6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'todo',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
--- ToDoList/todo/models.py
+++ ToDoList/todo/models.py
@@ -1,3 +1,14 @@
+# todo > models.py
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length = 100)          # Todo의 제목
+    description = models.TextField(blank = True)        # Todo에 대한 설명
+    created = models.DateTimeField(auto_now_add = True) # Todo 생성 일자 -> 스터디 메이트X
+    complete = models.BooleanField(default = False)     # Todo 완료 여부
+    important = models.BooleanField(default = False)    # Todo 중요 여부 -> 스터디 메이트X
+
+    # __str__: 내가 입력한 값 그대로를 출력하여 보고싶을 때 이 함수를 사용한다[모델 클래스의 객체를 문자열로]
+    def __str__(self):
+        return self.title
